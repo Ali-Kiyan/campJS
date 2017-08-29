4d4c233dea11858ba385c44f68cc33e52a2b45de
Ali <alikiyand@gmail.com>
2017-08-29 22:28:26 +0100

    img grid system

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
 
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+var campgrounds = [
+    {name: "Salmonela Mountain", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"}
+  ];
 
 app.get("/", function(req, res){
 
@@ -12,13 +37,24 @@ res.render("landing");
 
 app.get("/campgrounds", function(req, res){
 
-var campgrounds = [
-    {name: "Salmonela Mountain", image: "/images/1.jpg"},
-    {name: "Mountain Hill", image: "/images/2.jpg"},
-    {name: "Mountain Goat's Rest", image: "/images/3.jpg"}
-  ];
+
   res.render("campgrounds", {campgrounds : campgrounds});
 
+
+});
+
+app.get("/campgrounds/new", function(req, res){
+
+res.render("new.ejs");
+
+});
+
+app.post("/campgrounds", function(req, res){
+ var name = req.body.name;
+ var image = req.body.image;
+ var newCampground = {name: name, image: image}
+ campgrounds.push(newCampground);
+ res.redirect("campgrounds");
 
 });
 
