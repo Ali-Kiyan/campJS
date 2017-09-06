d9770faaffd3cbcd97d04247ff9a13a1da0cf09c
Ali <alikiyand@gmail.com>
2017-09-06 22:18:55 +0100

    adding authentication to post and get routes in campground for more security

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -18,7 +18,7 @@ router.get("/", function(req, res){
 
 });
 //CREATE - add new campground to DB
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
  var name = req.body.name;
  var image = req.body.image;
  var desc = req.body.description;
@@ -33,7 +33,7 @@ router.post("/", function(req, res){
  });
 });
 //NEW - Show form to create new campground
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
 
 res.render("campgrounds/new");
 
--- routes/comments.js
+++ routes/comments.js
@@ -28,7 +28,9 @@ router.post("/", isLoggedIn, function(req, res){
                console.log(err);
              }
              else{
-               req.user
+               comment.author.id = req.user._id;
+               comment.author.username = req.user.username;
+               comment.save();
                campground.comments.push(comment);
                campground.save();
                res.redirect("/campgrounds/" + campground._id)
