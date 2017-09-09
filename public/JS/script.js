73611c10fc75ad09a3d3b7524cd6ad0ca13d2de8
Ali <alikiyand@gmail.com>
2017-09-09 22:57:46 +0100

    adding UX

---
+++ public/JS/script.js
@@ -0,0 +1,5 @@
+var typed = new Typed('#jumbotron', {
+  strings: ["View our hand picked campgrounds from all over the world",""],
+  typeSpeed: 100
+});
+document.querySelectorAll(".typed-cursor")[0].style.display = "none";
