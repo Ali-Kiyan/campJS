e083c1df0eeb859b27db5681b5529351aff3da4b
Ali <alikiyand@gmail.com>
2017-10-21 17:40:33 +0100

    changing the typeJS text

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#jumbotron', {
-  strings: ["View campgrounds from all over the world","campJS"],
+  strings: ["View campgrounds from all over the world"," "],
   typeSpeed: 1000
 });
 document.querySelectorAll(".typed-cursor")[0].style.display = "none";
